417463408aa03fb53ef4923d9268db2457fc3ace
Mike McBride <mike.mcbride@hey.com>
2023-05-19 00:16:10 -0500

    adds updated svg logos, new header, brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
       center: true,
     },
     extend: {
-      colors: {},
+      colors: {
+      'brand-red': '#ff0057',
+      'brand-blue': '#0b2245',
+      'brand-yellow': '#ffce00',
+      'brand-gray': '#cfcfd1',
+      },
     },
   },
   variants: {},
